49138130f3019b85c31aed136d846eb0fad71364
junhe-c <cjunhe05@gmail.com>
2024-03-26 22:05:16 -0700

    Added toStringTest()

--- src/test/model/ExpenseTest.java
+++ src/test/model/ExpenseTest.java
@@ -30,4 +30,14 @@ public class ExpenseTest {
         assertEquals(burger.getAmount(), 5.25);
         assertNotEquals(burger.getAmount(), 5.2500001);
     }
+
+    @Test
+    public void toStringTest() {
+        assertEquals("Tesla Model Y - $100000.00", car.toString());
+        assertEquals("McChicken - $5.25", burger.toString());
+        Expense expense1 = new Expense("Test", 1.123);
+        assertEquals("Test - $1.12", expense1.toString());
+        Expense expense2 = new Expense("Test2", 1.126);
+        assertEquals("Test2 - $1.13", expense2.toString());
+    }
 }
